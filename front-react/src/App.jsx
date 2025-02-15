7bd85542a7a81b55534a52378caa3bf37ecbbb23
Rosa Jelves <rosa.sakura.j@gmail.com>
2025-02-15 12:23:42 -0300

    corrección se muestra el header

--- front-react/src/App.jsx
+++ front-react/src/App.jsx
@@ -7,16 +7,22 @@ import Footer from "./components/Footer";
 import "./styles/app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { useLocation } from 'react-router-dom';
 
 function App() {
   const {user} = useContext(UserContext);
+  const location = useLocation();
+  
+ // Definir rutas donde NO se debe mostrar el Header
+ const hiddenHeaderRoutes = ["/principal", "/profile", "/cart"];
 
   return (
     <>
       <div className="d-flex flex-column min-vh-100 all-body">
         <Navbar />
         <main className="flex-grow-1">
-          <Header />
+          {/* Si la ruta no está en hiddenHeaderRoutes, mostrar el Header */}
+          {!hiddenHeaderRoutes.includes(location.pathname) && <Header />} 
           <AppRoutes />
         </main>
         <Footer />
