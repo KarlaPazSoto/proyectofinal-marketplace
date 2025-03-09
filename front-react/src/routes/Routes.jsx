e6d616012228306597efd363555a6149ae3c84e5
Karla Paz Soto <ksoto.albornoz@gmail.com>
2025-03-09 02:07:52 -0300

    subida de cambios

--- front-react/src/routes/Routes.jsx
+++ front-react/src/routes/Routes.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
 import Home from '../pages/home/Home';
-import About from '../pages/about/About';
 import Register from '../pages/register/Register';
 import Login from '../pages/login/Login';
 import Profile from '../pages/profile/Profile';
@@ -22,18 +21,34 @@ import UserInfo from '../pages/profile/UserInfo';
 import PurchaseSummary from '../components/PurchaseSummary';
 import EditProfile from '../pages/profile/EditProfile';
 import SearchPage from '../pages/search/SearchPage';
+import AboutTradz from '../pages/footerPages/AboutTradz';
+import Careers from '../pages/footerPages/Careers';
+import Contact from '../pages/footerPages/Contact';
+import FAQ from '../pages/footerPages/FAQ';
+import Shipping from '../pages/footerPages/Shipping';
+import Returns from '../pages/footerPages/Returns';
+import Support from '../pages/footerPages/Support';
 
 function AppRoutes() {
   const { token } = useContext(UserContext);
 
   return (
     <Routes>
+          <Route path="/about" element={<AboutTradz />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/shipping" element={<Shipping />} />
+          <Route path="/returns" element={<Returns />} />
+          <Route path="/support" element={<Support />} />
+
       {/* Si NO está autenticado, solo puede ver /principal */}
       {!token ? (
         <>
           <Route path="/principal" element={<Principal />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
           {/* Si intenta acceder a cualquier otra ruta, lo redirige a /principal */}
           <Route path="*" element={<Navigate to="/principal" />} />
         </>
@@ -41,7 +56,6 @@ function AppRoutes() {
         <>
           {/* Si está autenticado, puede ver todas las rutas protegidas */}
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/carousel" element={<Carousel />} />
